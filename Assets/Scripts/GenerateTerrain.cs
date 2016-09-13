3eaa3bc4bab900a850e329adb708d0cac3a1adb3
Mengqi Ye <mengqi.ye@yungoal.com>
2016-09-13 17:20:47 +0800

    first commit

---
+++ Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    public int heightScale = 5;
+    public float detailScale = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + transform.position.x) / detailScale, 
+                                (vertices[i].z + transform.position.z) / detailScale) * heightScale;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        gameObject.AddComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
